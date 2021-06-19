8ac7af8773c38052573af1fa3aa4e649ff6bfd51
ViolaCello <ViolaCelloNYC@gmail.com>
2021-06-19 15:42:40 -0400

    iterative  binary_search Ruby

--- binarySeach.rb
+++ binarySeach.rb
@@ -11,3 +11,22 @@ def binary_search(query, arr)
     return binary_search(query, left) if arr[midpoint] > query
     return binary_search(query, right) if arr[midpoint] < query
 end
+
+def binary_search_iterative(query, arr)
+    while true do
+    return false if arr.length<=1 && (arr[0] == nil || arr[0] != query)
+    if arr.length==2
+        return true if arr[0]==query || arr[1]==query
+        return false
+    end
+    midpoint = (arr.length/2).floor
+    left = arr[0..midpoint-1]
+    right = arr[midpoint..arr.length]
+    return true if arr[midpoint]==query
+    if arr[midpoint] > query
+        arr = left
+    else 
+        arr = right
+    end
+end
+end
